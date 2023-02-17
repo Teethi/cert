06f0eebf32b23cc0b98479b584a8dcce64a194a5
kenilshahh <kenilshah1505@gmail.com>
2023-02-17 21:39:50 +0530

    hgturfdh

---
+++ contracts/CertificateRegistry.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+//B-CELO
+contract CertificateRegistry {
+    // Define the struct to store certificate information
+    struct Certificate {
+        string name;
+        string uniName;
+        string month;
+         string course;
+        uint256 year;
+       
+        bool isApproved;
+        bool isApplied;
+        address user;
+    }
+
+    // Mapping to store certificate information
+    mapping(address => Certificate) public certificates;
+
+    // Event to emit upon approval of a certificate
+    event CertificateApproved(address user, uint256 serialNumber);
+
+    // Function for users to apply for a certificate
+    function applyForCertificate(
+        string memory name,
+        string memory uniName,
+        string memory course,
+        string memory month,
+        uint256 year,
+        bool isApproved,
+        bool isApplied,
+        address user
+    ) public {
+        user= msg.sender;
+        certificates[msg.sender] = Certificate(name, uniName, course, month, year, false, true, user);
+    }
+
+    // Function for the institution/organization to approve a certificate
+    function approveCertificate(
+        bool isApproved,address user
+    ) public {
+        Certificate storage certificate = certificates[user];
+        require(
+            !certificate.isApproved,
+            "Certificate has already been approved"
+        );
+        certificate.isApproved = true;
+        certificate.user = user;
+        // emit CertificateApproved(user);
+    }
+ 
+    // Function for users to get their certificate serial number and QR code
+    function getCertificateInfo() public view returns (string memory, string memory, string memory, string memory, uint256,bool, bool, address) {
+        Certificate storage certificate = certificates[msg.sender];
+        require(
+            certificate.isApproved,
+            "Certificate has not been approved yet"
+        );
+        // bytes32 qrCode = keccak256(
+        //     abi.encodePacked(certificate.serialNumber, msg.sender)
+        // );
+        return (certificate.name,certificate.uniName, certificate.course, certificate.month, certificate.year, true, false, msg.sender);
+    }
+
+    // Function for organizations to verify a certificate
+    // function verifyCertificate(
+    //     uint256 serialNumber,
+    //     address user
+    // ) public view returns (bool) {
+    //     Certificate storage certificate = certificates[user];
+    //     return
+    //         certificate.isApproved && certificate.serialNumber == serialNumber;
+    // }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+/** @format */
+
+require('@nomiclabs/hardhat-waffle');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: 'hardhat',
+  paths: {
+    artifacts: './artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    celo: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/UVQD7tAU7RwKHWZ7Pv3E7nbwMAJEsWLF',
+      accounts: [
+        '8cf4c3f797a292fc7d614699cdb22e58e4b7569ae2fa2bb95a8409e864ebb0ed',
+      ]
+    },
+  },
+  solidity: '0.8.4',
+};
